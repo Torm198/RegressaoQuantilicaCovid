04f4da64dcae7554ef4dbcdb88ab8fefc9c7a1e2
Gabriel Tormin <57624605+Torm198@users.noreply.github.com>
2022-04-19 20:01:38 -0300

    Update Codigo Gráficos.R

--- Codigo Gráficos.R
+++ Codigo Gráficos.R
@@ -12,20 +12,23 @@ periodo3 <- read_csv('Cortes/Periodo 3/Banco 01.12.2021 a 17.03.2022.csv')
 graf1 <- left_join(mun_sp,periodo1,by=c('cod_ibge'='Codigo'))
 graf2 <- left_join(mun_sp,periodo2,by=c('cod_ibge'='Codigo'))
 graf3 <- left_join(mun_sp,periodo3,by=c('cod_ibge'='Codigo'))
-
+graf_all <- left_join(mun_sp,todo_periodo,by=c('cod_ibge'='Codigo'))
 #labs(title="Taxa de Letalidade por município em São Paulo até 04/10/2021", size=8)+
 
 
 # letalidade
 ggplot()+geom_sf(data = graf1,aes(fill=let),size=.15)+
-  scale_fill_gradient(name = "Taxa de letalidade",low="lightgrey", high="red",na.value = "white",limits=c(0,1))  + 
+  scale_fill_gradient(name = "Taxa de letalidade",low="lightgrey", high="red",na.value = "white")  + 
   theme_minimal(base_size = 10)
 ggplot()+geom_sf(data = graf2,aes(fill=let),size=.15)+
   scale_fill_gradient(name = "Taxa de letalidade",low="lightgrey", high="red",na.value = "white",limits=c(0,1))  + 
   theme_minimal(base_size = 10)
 ggplot()+geom_sf(data = graf3,aes(fill=let),size=.15)+
-  scale_fill_gradient(name = "Taxa de letalidade",low="lightgrey", high="red",na.value = "white",limits=c(0,1))  + 
+  scale_fill_gradient(name = "Taxa de letalidade",low="lightgrey", high="red",na.value = "white",limits=c(0,.25))  + 
   theme_minimal(base_size = 10)
+ggplot()+geom_sf(data = graf_all,aes(fill=let),size=.15)+
+  scale_fill_gradient(name = "Taxa de letalidade",low="lightgrey", high="red",na.value = "white",limits=c(0,.25))  + 
+  theme_minimal(base_size = 10)+facet_wrap(~periodo)
 
 
 
@@ -50,6 +53,24 @@ ggplot()+geom_sf(data = graf3,aes(fill=Idade_mediana),size=.15)+
 
 
 
+ggplot()+geom_sf(data = ,aes(fill=Idade_mediana),size=.15)+
+  scale_fill_gradient(name = "Idade",low="lightgrey", high="red",na.value = "white")  + 
+  theme_minimal(base_size = 10) +facet_wrap(~periodo)
+
+
+ggplot()+geom_sf(data = graf_all ,aes(fill=dose1),size=.15)+
+  scale_fill_gradient(name = "Porcentagem de doses",low="lightgrey", high="red",na.value = "white")  + 
+  theme_minimal(base_size = 10) +facet_wrap(~periodo)
+ggplot()+geom_sf(data = graf_all ,aes(fill=dose2),size=.15)+
+  scale_fill_gradient(name = "Porcentagem de doses",low="lightgrey", high="red",na.value = "white")  + 
+  theme_minimal(base_size = 10) +facet_wrap(~periodo)
+ggplot()+geom_sf(data = graf_all ,aes(fill=reforco),size=.15)+
+  scale_fill_gradient(name = "Porcentagem de doses",low="lightgrey", high="red",na.value = "white")  + 
+  theme_minimal(base_size = 10) +facet_wrap(~periodo)
+
+
+
+
 #### let x y
 todo_periodo <- 
   bind_rows(
@@ -58,6 +79,8 @@ todo_periodo <-
   periodo3 %>% mutate(periodo=3))
 
 ggplot(todo_periodo,aes(x=IDHM,y=let)) + geom_point() +facet_wrap(~periodo)
+
+
 ggplot(todo_periodo,aes(x=Idade_mediana,y=let)) + geom_point() +facet_wrap(~periodo)
 ggplot(todo_periodo,aes(x=densidade2021,y=let)) + geom_point() +facet_wrap(~periodo)
 ggplot(todo_periodo,aes(x=Risco,y=let)) + geom_point() +facet_wrap(~periodo)
@@ -65,3 +88,12 @@ ggplot(todo_periodo,aes(x=dose1,y=let)) + geom_point() +facet_wrap(~periodo)
 ggplot(todo_periodo,aes(x=dose2,y=let)) + geom_point() +facet_wrap(~periodo)
 ggplot(todo_periodo,aes(x=reforco,y=let)) + geom_point() +facet_wrap(~periodo)
 
+################
+
+
+ggplot(todo_periodo,aes(x=let)) + geom_histogram() +facet_wrap(~periodo)
+ggplot(todo_periodo,aes(x=Risco)) + geom_histogram() +facet_wrap(~periodo)
+ggplot(todo_periodo,aes(x=Idade_mediana)) + geom_histogram() +facet_wrap(~periodo)
+ggplot(todo_periodo,aes(x=let)) + geom_histogram() +facet_wrap(~periodo)
+ggplot(periodo1,aes(x=IDHM)) + geom_histogram()
+ggplot(periodo1,aes(x=densidade2021)) + geom_histogram()
